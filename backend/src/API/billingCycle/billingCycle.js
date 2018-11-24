bb31da175bf450b033802d2491f5f24fdf4c6efa
Erislandio Soares <erislandiosoares21@gmail.com>
2018-11-24 17:08:43 -0200

    Adição da rota summary

--- backend/src/API/billingCycle/billingCycle.js
+++ backend/src/API/billingCycle/billingCycle.js
@@ -2,13 +2,13 @@ const restful = require('node-restful')
 const mongoose = restful.mongoose
 
 const creditSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    value: { type: Number, required: true, min: 0 }
+    name: { type: String, required: [true, 'Informe o nome para o tipo credito'] },
+    value: { type: Number, required: [true, 'Informe o valor do credito'], min: 0 }
 })
 
 const debtSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    value: { type: Number, min: 0, required: true },
+    name: { type: String, required: [true, 'Informe o nome para o tipo do debito'] },
+    value: { type: Number, min: 0, required: [true, 'Informe o valor do debito'] },
     status: {
         type: String, required: false, uppercase: true,
         enum: ['PAGO', 'PRENENTE', 'AGENDADO']
@@ -19,8 +19,8 @@ const billingCycleSchema = new mongoose.Schema({
     name: { type: String, required: true },
     month: { type: Number, min: 1, max: 12, required: true },
     year: { type: Number, min: 1970, max: 2100 },
-    credit: [creditSchema],
-    debts: [debtSchema]
+    credits: [creditSchema],
+    debits: [debtSchema]
 })
 
 module.exports = restful.model('BillingCycle', billingCycleSchema)
--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -2,4 +2,9 @@ const mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
 
-module.exports =  mongoose.connect('mongodb://localhost/mymoney', { useNewUrlParser: true })
+module.exports = mongoose.connect('mongodb://localhost/mymoney', { useNewUrlParser: true })
+
+mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatorio"
+mongoose.Error.messages.Number.min = " O valor '{PATH} informado é menor que o limite de '{MIN}'"
+mongoose.Error.messages.Number.max = " O valor '{PATH} informado é maior que o limite de '{MAX}'"
+mongoose.Error.messages.String.enum = "'{VALUE}' não é valido para o atributo '{PATH}'"

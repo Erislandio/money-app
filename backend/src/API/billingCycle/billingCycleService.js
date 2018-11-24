ed49e0481b1dc8daae04bb3b5a760612d422f6ee
Erislandio Soares <erislandiosoares21@gmail.com>
2018-11-24 17:18:29 -0200

    Adição do tratamento de erros entre as requisiççoes

--- backend/src/API/billingCycle/billingCycleService.js
+++ backend/src/API/billingCycle/billingCycleService.js
@@ -1,32 +1,34 @@
 const BillingCycle = require('./billingCycle')
+const errorHandle = require('../commom/errorHandler')
 
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
-BillingCycle.updateOptions({new: true, runValidators: true})
+BillingCycle.updateOptions({ new: true, runValidators: true })
+BillingCycle.after('post', errorHandle).after('put', errorHandle)
 
 BillingCycle.route('count', (req, res, next) => {
     BillingCycle.count((error, value) => {
-        if(error) {
-            res.status(500).json({errors: [error]})
+        if (error) {
+            res.status(500).json({ errors: [error] })
         } else {
-            res.json({value})
+            res.json({ value })
         }
     })
 })
 
 BillingCycle.route('summary', (req, res, next) => {
     BillingCycle.aggregate({
-        $project: {credit: {$sum: "$credits.value"}, debt: {$sum: "$debits.value"}}
+        $project: { credit: { $sum: "$credits.value" }, debt: { $sum: "$debits.value" } }
     }, {
-        $group: {_id: null, credit: {$sum: "$credit"}, debt: {$sum: "$debt"}}
-    }, {
-        $project: {_id: 0, credit: 1, debt: 1}
-    }, (error, result) => {
-        if(error) {
-            res.status(500).json({errors: [error]})
-        } else {
-            res.json(result[0] || { credit: 0, debt: 0 })
-        }
-    })
+            $group: { _id: null, credit: { $sum: "$credit" }, debt: { $sum: "$debt" } }
+        }, {
+            $project: { _id: 0, credit: 1, debt: 1 }
+        }, (error, result) => {
+            if (error) {
+                res.status(500).json({ errors: [error] })
+            } else {
+                res.json(result[0] || { credit: 0, debt: 0 })
+            }
+        })
 })
 
 module.exports = BillingCycle
